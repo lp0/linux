306021b70fbb819968d37769cc28ef9cda3025f9
Simon Arlott <sa.me.uk>
2012-05-11 19:05:58 +0100

    remove the PM code that doesn't do anything

--- arch/arm/mach-bcm2708/armctrl.c
+++ arch/arm/mach-bcm2708/armctrl.c
@@ -19,7 +19,6 @@
 #include <linux/list.h>
 #include <linux/io.h>
 #include <linux/version.h>
-#include <linux/syscore_ops.h>
 #include <linux/interrupt.h>
 #include <linux/slab.h>
 #include <linux/of_address.h>
@@ -73,107 +72,12 @@ static void armctrl_unmask_irq(struct irq_data *d)
 	writel(BIT(d->hwirq), data->enable);
 }
 
-#if defined(CONFIG_PM)
-
-/* Static defines
- * @irq: The IRQ number for the base of the VIC.
- * @base: The register base for the VIC.
- * @resume_sources: A bitmask of interrupts for resume.
- * @resume_irqs: The IRQs enabled for resume.
- * @int_select: Save for VIC_INT_SELECT.
- * @int_enable: Save for VIC_INT_ENABLE.
- * @soft_int: Save for VIC_INT_SOFT.
- * @protect: Save for VIC_PROTECT.
- */
-struct armctrl_info {
-	void __iomem *base;
-	int irq;
-	u32 resume_sources;
-	u32 resume_irqs;
-	u32 int_select;
-	u32 int_enable;
-	u32 soft_int;
-	u32 protect;
-} armctrl;
-
-static int armctrl_suspend(void)
-{
-	return 0;
-}
-
-static void armctrl_resume(void)
-{
-	return;
-}
-
-
-/**
- * armctrl_pm_register - Register a VIC for later power management control
- * @base: The base address of the VIC.
- * @irq: The base IRQ for the VIC.
- * @resume_sources: bitmask of interrupts allowed for resume sources.
- */
-static void __init armctrl_pm_register(void __iomem * base, unsigned int irq,
-				       u32 resume_sources)
-{
-	armctrl.base = base;
-	armctrl.resume_sources = resume_sources;
-	armctrl.irq = irq;
-}
-
-static int armctrl_set_wake(struct irq_data *d, unsigned int on)
-{
-	unsigned int off = d->irq & 31;
-	u32 bit = BIT(off);
-
-	if (!(bit & armctrl.resume_sources))
-		return -EINVAL;
-
-	if (on)
-		armctrl.resume_irqs |= bit;
-	else
-		armctrl.resume_irqs &= ~bit;
-
-	return 0;
-}
-
-#else
-static inline void armctrl_pm_register(void __iomem *base, unsigned int irq,
-				       u32 arg1)
-{
-}
-#define armctrl_suspend NULL
-#define armctrl_resume NULL
-#define armctrl_set_wake NULL
-#endif /* CONFIG_PM */
-
-static struct syscore_ops armctrl_syscore_ops = {
-	.suspend = armctrl_suspend,
-	.resume = armctrl_resume,
-};
-
-/**
- * armctrl_syscore_init - initicall to register VIC pm functions
- *
- * This is called via late_initcall() to register
- * the resources for the VICs due to the early
- * nature of the VIC's registration.
-*/
-static int __init armctrl_syscore_init(void)
-{
-	register_syscore_ops(&armctrl_syscore_ops);
-	return 0;
-}
-
-late_initcall(armctrl_syscore_init);
-
 static struct irq_chip armctrl_chip = {
 	.name = "ARMCTRL-level",
 	.irq_ack = armctrl_mask_irq,
 	.irq_mask = armctrl_mask_irq,
 	.irq_mask_ack = armctrl_mask_irq,
-	.irq_unmask = armctrl_unmask_irq,
-	.irq_set_wake = armctrl_set_wake,
+	.irq_unmask = armctrl_unmask_irq
 };
 
 static u32 of_read_u32(const struct device_node *np, const char *name, u32 def)
