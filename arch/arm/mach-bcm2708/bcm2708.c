be81bbec875c87b915ab93375f04a2dad62239de
Simon Arlott <sa.me.uk>
2012-05-12 20:27:48 +0100

    Use clocksource_mmio_dt
    Signed-off-by: Simon Arlott <sa.me.uk>

--- arch/arm/mach-bcm2708/bcm2708.c
+++ arch/arm/mach-bcm2708/bcm2708.c
@@ -21,7 +21,7 @@
 #include <linux/syscore_ops.h>
 #include <linux/interrupt.h>
 #include <linux/clockchips.h>
-#include <linux/cnt32_to_63.h>
+#include <linux/clocksource.h>
 #include <linux/io.h>
 #include <linux/module.h>
 #include <linux/of_platform.h>
@@ -34,7 +34,6 @@
 #include <mach/hardware.h>
 #include <asm/irq.h>
 #include <asm/mach-types.h>
-#include <asm/sched_clock.h>
 #include <asm/exception.h>
 
 #include <asm/mach/arch.h>
@@ -90,38 +89,6 @@ static struct clk_lookup lookups[] = {
 	}
 };
 
-
-/* The STC is a free running counter that increments at the rate of 1MHz */
-#define STC_FREQ_HZ 1000000
-
-static cycle_t stc_read_cycles(struct clocksource *cs)
-{
-	/* STC: a free running counter that increments at the rate of 1MHz */
-	return (cycle_t)readl(__io_address(ST_BASE+0x04));
-}
-
-static struct clocksource clocksource_stc = {
-	.name	= "stc",
-	.rating	= 300,
-	.read	= stc_read_cycles,
-	.mask	= CLOCKSOURCE_MASK(32),
-	.flags	= CLOCK_SOURCE_IS_CONTINUOUS,
-};
-
-static void __init bcm2708_clocksource_init(void)
-{
-	/* calculate .shift and .mult values and register clocksource */
-	if (clocksource_register_hz(&clocksource_stc, STC_FREQ_HZ)) {
-		printk(KERN_ERR "timer: failed to initialize clock source %s\n",
-				clocksource_stc.name);
-	}
-}
-
-u32 notrace bcm2708_read_sched_clock(void)
-{
-	return clocksource_stc.read(&clocksource_stc);
-}
-
 void __init bcm2708_init(void)
 {
 	int ret;
@@ -140,98 +107,19 @@ void __init bcm2708_init(void)
 	}
 }
 
-#define TIMER_PERIOD 10000	/* HZ in microsecs */
-
-static void timer_set_mode(enum clock_event_mode mode,
-		struct clock_event_device *clk)
-{
-	unsigned long stc;
-
-	switch (mode) {
-	case CLOCK_EVT_MODE_PERIODIC:
-		stc = readl(__io_address(ST_BASE + 0x04));
-		/* stc3 */
-		writel(stc + TIMER_PERIOD, __io_address(ST_BASE + 0x18));
-		break;
-	case CLOCK_EVT_MODE_ONESHOT:
-	case CLOCK_EVT_MODE_UNUSED:
-	case CLOCK_EVT_MODE_SHUTDOWN:
-		break;
-	default:
-		printk(KERN_ERR "timer_set_mode: unhandled mode: %d\n",
-				(int)mode);
-		break;
-	}
-}
-
-static int timer_set_next_event(unsigned long cycles,
-		struct clock_event_device *unused)
-{
-	unsigned long stc;
-
-	stc = readl(__io_address(ST_BASE + 0x04));
-	writel(stc + cycles, __io_address(ST_BASE + 0x18)); /* stc3 */
-	return 0;
-}
-
-static struct clock_event_device timer0 = {
-	.name = "timer0",
-	.shift = 32,
-	.features = CLOCK_EVT_FEAT_ONESHOT,
-	.set_mode = timer_set_mode,
-	.set_next_event = timer_set_next_event,
-};
-
-/*
- * IRQ handler for the timer
- */
-static irqreturn_t bcm2708_timer_interrupt(int irq, void *dev_id)
-{
-	struct clock_event_device *evt = &timer0;
-
-	/* stcs clear timer int */
-	writel(1 << 3, __io_address(ST_BASE + 0x00));
-
-	evt->event_handler(evt);
-
-	return IRQ_HANDLED;
-}
-
-static struct irqaction bcm2708_timer_irq = {
-	.name = "BCM2708 Timer Tick",
-	.flags = IRQF_DISABLED | IRQF_TIMER | IRQF_IRQPOLL,
-	.handler = bcm2708_timer_interrupt,
-};
-
-/*
- * Set up timer interrupt, and return the current time in seconds.
- */
 static void __init bcm2708_timer_init(void)
 {
-	/* init high res timer */
-	bcm2708_clocksource_init();
-
 	/* Initialise to a known state (all timers off) */
 	writel(0, __io_address(ARM_T_CONTROL));
 
-	/* Make irqs happen for the system timer */
-	setup_irq(IRQ_TIMER3, &bcm2708_timer_irq);
-
-	setup_sched_clock(bcm2708_read_sched_clock, 32, STC_FREQ_HZ);
-
-	timer0.mult = div_sc(STC_FREQ_HZ, NSEC_PER_SEC, timer0.shift);
-	timer0.max_delta_ns = clockevent_delta2ns(0xffffffff, &timer0);
-	timer0.min_delta_ns = clockevent_delta2ns(0xf, &timer0);
-
-	timer0.cpumask = cpumask_of(0);
-	clockevents_register_device(&timer0);
+	clockevent_mmio_dt_init();
 }
 
 struct sys_timer bcm2708_timer = {
 	.init = bcm2708_timer_init
 };
 
-static const char *bcm2708_compat[] __initdata = {
+static const char *bcm2708_compat[] = {
 	"broadcom,bcm2708",
 	NULL
 };
