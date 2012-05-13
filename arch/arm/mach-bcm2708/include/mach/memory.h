43ea1af22f93dfd1cebead317328aad9b6ba9704
Simon Arlott <sa.me.uk>
2012-05-13 17:22:39 +0100

    add more details of the memory layout

--- arch/arm/mach-bcm2708/include/mach/memory.h
+++ arch/arm/mach-bcm2708/include/mach/memory.h
@@ -19,24 +19,27 @@
 #define __ASM_ARCH_MEMORY_H
 
 /* Memory overview:
-
-   [ARMcore] <--virtual addr-->
-   [ARMmmu] <--physical addr-->
-   [GERTmap] <--bus add-->
-   [VCperiph]
-
-*/
-
-/*
- * Physical DRAM offset.
+ * [ARMcore] <--virtual addr--> [ARMmmu] <--physical addr--> [GERTmap] <--bus add--> [VCperiph]
  */
-#define PLAT_PHYS_OFFSET UL(0x00000000)
-#define VC_ARMMEM_OFFSET UL(0x00000000)   /* offset in VC of ARM memory */
+
+/* Physical DRAM offset.
+ */
+#define PLAT_PHYS_OFFSET	UL(0x00000000)
+#define VC_ARMMEM_OFFSET	UL(0x00000000)	/* offset in VC of ARM memory */
+
+/* VC memory aliases:
+ */
+#define VC_BUS_DIRECT_UNCACHED	UL(0xC0000000)	/* direct uncached */
+#define VC_BUS_L2_CACHE_ONLY	UL(0x80000000)	/* L2 cached (only) */
+#define VC_BUS_L2_CACHE		UL(0x40000000)	/* L2 cache coherent (non allocating) */
+#define VC_BUS_L1_AND_L2_CACHED	UL(0x00000000)	/* L1 and L2 cached */
 
 #ifdef CONFIG_BCM2708_NOL2CACHE
- #define _REAL_BUS_OFFSET UL(0xC0000000)   /* don't use L1 or L2 caches */
+ /* don't use L1 or L2 caches */
+ #define VC_BUS_OFFSET		VC_BUS_DIRECT_UNCACHED
 #else
- #define _REAL_BUS_OFFSET UL(0x40000000)   /* use L2 cache */
+ /* use L2 cache */
+ #define VC_BUS_OFFSET		VC_BUS_L2_CACHE
 #endif
 
 /* We're using the memory at 64M in the VideoCore for Linux - this adjustment
@@ -46,17 +49,15 @@
  * WARNING: this only works because the ARM is given memory at a fixed location
  *          (VC_ARMMEM_OFFSET)
  */
-#define BUS_OFFSET          (VC_ARMMEM_OFFSET + _REAL_BUS_OFFSET)
-#define __virt_to_bus(x)    ((x) + (BUS_OFFSET - PAGE_OFFSET))
-#define __bus_to_virt(x)    ((x) - (BUS_OFFSET - PAGE_OFFSET))
-#define __pfn_to_bus(x)     (__pfn_to_phys(x) + (BUS_OFFSET - PLAT_PHYS_OFFSET))
-#define __bus_to_pfn(x)     __phys_to_pfn((x) - (BUS_OFFSET - PLAT_PHYS_OFFSET))
+#define BUS_OFFSET		(VC_ARMMEM_OFFSET + VC_BUS_OFFSET)
+#define __virt_to_bus(x)	((x) + (BUS_OFFSET - PAGE_OFFSET))
+#define __bus_to_virt(x)	((x) - (BUS_OFFSET - PAGE_OFFSET))
+#define __pfn_to_bus(x)		(__pfn_to_phys(x) + (BUS_OFFSET - PLAT_PHYS_OFFSET))
+#define __bus_to_pfn(x)		__phys_to_pfn((x) - (BUS_OFFSET - PLAT_PHYS_OFFSET))
 
 /*
- * Consistent DMA area set to 2M. Framebuffer now allocated on host
+ * Consistent DMA area set to 2M. Framebuffer now allocated on host.
  */
-
-
-#define CONSISTENT_DMA_SIZE 0x00200000
+#define CONSISTENT_DMA_SIZE	0x00200000
 
 #endif
