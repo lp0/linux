34b79d6a6c412705e791c341768d6ea893f36bdf
Simon Arlott <sa.me.uk>
2012-05-14 03:42:48 +0100

    [rpi02] bcm-mbox: Add Broadcom VideoCore mailbox driver
    Quite an interesting interface... with a very small message size
    
    Signed-off-by: Simon Arlott <sa.me.uk>

---
+++ drivers/misc/bcm-mbox.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2010 Broadcom
+ * Copyright 2012 Simon Arlott
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _BCM_MBOX_H
+#define _BCM_MBOX_H
+
+struct bcm_mbox_chan;
+
+/*
+ * @node: calling node
+ * @pmbox: property name for mailbox phandle
+ * @pchan: property name for channel index
+ *
+ * Returns an error pointer if not found, otherwise holds a reference to the
+ * device that must be put back with bcm_mbox_put.
+ */
+extern struct bcm_mbox_chan *bcm_mbox_get(struct device_node *node,
+	const char *pmbox, const char *pchan);
+extern char *bcm_mbox_name(struct bcm_mbox_chan *mbox);
+extern void bcm_mbox_put(struct bcm_mbox_chan *mbox);
+
+extern int bcm_mbox_poll(struct bcm_mbox_chan *mbox, u32 *data28);
+extern int bcm_mbox_read(struct bcm_mbox_chan *mbox, u32 *data28);
+extern int bcm_mbox_read_interruptible(struct bcm_mbox_chan *mbox, u32 *data28);
+extern int bcm_mbox_read_timeout(struct bcm_mbox_chan *mbox, u32 *data28,
+	long jiffies);
+
+extern int bcm_mbox_write(struct bcm_mbox_chan *mbox, u32 data28);
+extern int bcm_mbox_call(struct bcm_mbox_chan *mbox,
+	u32 out_data28, u32 *in_data28);
+extern int bcm_mbox_call_interruptible(struct bcm_mbox_chan *mbox,
+	u32 out_data28, u32 *in_data28);
+extern int bcm_mbox_call_timeout(struct bcm_mbox_chan *mbox, u32 out_data28,
+	u32 *in_data28, long jiffies);
+
+extern int bcm_mbox_clear(struct bcm_mbox_chan *mbox);
+
+#endif
