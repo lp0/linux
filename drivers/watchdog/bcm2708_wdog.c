67489404715ed68d0314cd308796feb86f8cb918
Simon Arlott <sa.me.uk>
2012-05-15 18:22:21 +0100

    bcm2708_wdog: find watchdog for reboot using for_each_matching_node
    Avoids the need to maintain our own list
    
    Signed-off-by: Simon Arlott <sa.me.uk>

--- drivers/watchdog/bcm2708_wdog.c
+++ drivers/watchdog/bcm2708_wdog.c
@@ -19,6 +19,7 @@
 #include <linux/moduleparam.h>
 #include <linux/mutex.h>
 #include <linux/of_address.h>
+#include <linux/of_platform.h>
 #include <linux/platform_device.h>
 #include <linux/slab.h>
 #include <linux/types.h>
@@ -73,27 +74,14 @@ MODULE_PARM_DESC(nowayout,
 	"Watchdog cannot be stopped once started (default="
 				__MODULE_STRING(WATCHDOG_NOWAYOUT) ")");
 
-static LIST_HEAD(wdogs);
-static DEFINE_MUTEX(lock);
-
-static void bcm2708_wdog_lock(struct bcm2708_wdog *wdog)
-{
-	mutex_lock(&wdog->lock);
-}
-
-static void bcm2708_wdog_unlock(struct bcm2708_wdog *wdog)
-{
-	mutex_unlock(&wdog->lock);
-}
-
 static int bcm2708_wdog_start(struct watchdog_device *dev)
 {
 	struct bcm2708_wdog *wdog = watchdog_get_drvdata(dev);
 	u32 cur;
 
-	bcm2708_wdog_lock(wdog);
+	mutex_lock(&wdog->lock);
 	if (wdog->blocked) {
-		bcm2708_wdog_unlock(wdog);
+		mutex_unlock(&wdog->lock);
 		return -EBUSY;
 	}
 
@@ -108,7 +96,7 @@ static int bcm2708_wdog_start(struct watchdog_device *dev)
 		wdog->started = true;
 	}
 
-	bcm2708_wdog_unlock(wdog);
+	mutex_unlock(&wdog->lock);
 	return 0;
 }
 
@@ -116,9 +104,9 @@ static int bcm2708_wdog_stop(struct watchdog_device *dev)
 {
 	struct bcm2708_wdog *wdog = watchdog_get_drvdata(dev);
 
-	bcm2708_wdog_lock(wdog);
+	mutex_lock(&wdog->lock);
 	if (wdog->blocked) {
-		bcm2708_wdog_unlock(wdog);
+		mutex_unlock(&wdog->lock);
 		return -EBUSY;
 	}
 
@@ -129,7 +117,7 @@ static int bcm2708_wdog_stop(struct watchdog_device *dev)
 		wdog->started = false;
 	}
 
-	bcm2708_wdog_unlock(wdog);
+	mutex_unlock(&wdog->lock);
 	return 0;
 }
 
@@ -138,36 +126,16 @@ static unsigned int bcm2708_wdog_get_timeleft(struct watchdog_device *dev)
 	struct bcm2708_wdog *wdog = watchdog_get_drvdata(dev);
 	unsigned int remaining;
 
-	bcm2708_wdog_lock(wdog);
+	mutex_lock(&wdog->lock);
 	if (wdog->blocked) {
-		bcm2708_wdog_unlock(wdog);
+		mutex_unlock(&wdog->lock);
 		return -EBUSY;
 	}
 
 	remaining = WDOG_TICKS_TO_SECS(readl_relaxed(wdog->pm + PM_WDOG) & PM_WDOG_TIME_SET);
 
-	bcm2708_wdog_unlock(wdog);
+	mutex_unlock(&wdog->lock);
 	return remaining;
-}
-
-void bcm2708_wdog_restart(char str, const char *cmd)
-{
-	struct bcm2708_wdog *wdog;
-	u32 cur;
-
-	list_for_each_entry(wdog, &wdogs, list) {
-		bcm2708_wdog_lock(wdog);
-		dev_info(wdog->dev, "firing watchdog\n");
-
-		/* use a timeout of 10 ticks (~150us) */
-		writel_relaxed(10 | PM_PASSWORD, wdog->pm + PM_WDOG);
-		cur = readl_relaxed(wdog->pm + PM_RSTC);
-		writel_relaxed(PM_PASSWORD | (cur & PM_RSTC_WRCFG_CLR)
-			| PM_RSTC_WRCFG_FULL_RESET, wdog->pm + PM_RSTC);
-
-		wdog->blocked = true;
-		bcm2708_wdog_unlock(wdog);
-	}
 }
 
 static struct watchdog_ops bcm2708_ops = {
@@ -247,7 +215,6 @@ static int __devinit bcm2708_wdog_probe(struct platform_device *of_dev)
 
 	dev_info(wdog->dev, "at MMIO %#lx\n",
 		(unsigned long)wdog->res.start);
-	list_add_tail(&wdog->list, &wdogs);
 	platform_set_drvdata(of_dev, dev);
 	return 0;
 
@@ -263,7 +230,6 @@ static int __devexit bcm2708_wdog_remove(struct platform_device *of_dev)
 	struct bcm2708_wdog *wdog = watchdog_get_drvdata(dev);
 
 	watchdog_unregister_device(dev);
-	list_del(&wdog->list);
 	release_region(wdog->res.start, resource_size(&wdog->res));
 	kfree(wdog);
 	kfree(dev);
@@ -271,11 +237,49 @@ static int __devexit bcm2708_wdog_remove(struct platform_device *of_dev)
 	return 0;
 }
 
-static struct of_device_id bcm2708_wdog_dt_match[] __devinitconst = {
+static const struct of_device_id bcm2708_wdog_dt_match[] = {
 	{ .compatible = "broadcom,bcm2708-pm-wdog" },
 	{}
 };
 MODULE_DEVICE_TABLE(of, bcm2708_wdog_dt_match);
+
+void bcm2708_wdog_restart(char str, const char *cmd)
+{
+	struct device_node *node;
+	struct platform_device *pdev;
+	struct watchdog_device *dev;
+	struct bcm2708_wdog *wdog;
+	u32 cur;
+
+	for_each_matching_node(node, bcm2708_wdog_dt_match) {
+		pdev = of_find_device_by_node(node);
+		if (pdev == NULL)
+			continue;
+
+		dev = platform_get_drvdata(pdev);
+		if (dev == NULL)
+			goto put_node;
+
+		wdog = watchdog_get_drvdata(dev);
+		if (wdog == NULL)
+			goto put_node;
+
+		mutex_lock(&wdog->lock);
+		dev_info(wdog->dev, "firing watchdog\n");
+
+		/* use a timeout of 10 ticks (~150us) */
+		writel_relaxed(10 | PM_PASSWORD, wdog->pm + PM_WDOG);
+		cur = readl_relaxed(wdog->pm + PM_RSTC);
+		writel_relaxed(PM_PASSWORD | (cur & PM_RSTC_WRCFG_CLR)
+			| PM_RSTC_WRCFG_FULL_RESET, wdog->pm + PM_RSTC);
+
+		wdog->blocked = true;
+		mutex_unlock(&wdog->lock);
+
+put_node:
+		of_node_put(node);
+	}
+}
 
 static struct platform_driver bcm2708_wdog_driver = {
 	.probe = bcm2708_wdog_probe,
