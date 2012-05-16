78192dc73cb44e353ca7e6414cb4846b6b519835
Simon Arlott <sa.me.uk>
2012-05-16 23:10:51 +0100

    mmio-dt: include bitops.h
    Signed-off-by: Simon Arlott <sa.me.uk>

--- drivers/clocksource/mmio_dt.c
+++ drivers/clocksource/mmio_dt.c
@@ -18,6 +18,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include <linux/bitops.h>
 #include <linux/clocksource.h>
 #include <linux/clockchips.h>
 #include <linux/err.h>
