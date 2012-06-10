94e815c5eccb3ee1ca36133df52457f89a8487ca
Simon Arlott <sa.me.uk>
2012-06-10 10:53:33 +0100

    [rpi03] sdhci: don't use unlikely()
    If CONFIG_MMC_SDHCI_IO_ACCESSORS is Y then it's actually
    quite likely (but not guaranteed) that the host will use
    these ops on every access.
    
    Signed-off-by: Simon Arlott <sa.me.uk>

--- drivers/mmc/host/sdhci.h
+++ drivers/mmc/host/sdhci.h
@@ -285,7 +285,7 @@ struct sdhci_ops {
 
 static inline void sdhci_writel(struct sdhci_host *host, u32 val, int reg)
 {
-	if (unlikely(host->ops->write_l))
+	if (host->ops->write_l)
 		host->ops->write_l(host, val, reg);
 	else
 		writel(val, host->ioaddr + reg);
@@ -293,7 +293,7 @@ static inline void sdhci_writel(struct sdhci_host *host, u32 val, int reg)
 
 static inline void sdhci_writew(struct sdhci_host *host, u16 val, int reg)
 {
-	if (unlikely(host->ops->write_w))
+	if (host->ops->write_w)
 		host->ops->write_w(host, val, reg);
 	else
 		writew(val, host->ioaddr + reg);
@@ -301,7 +301,7 @@ static inline void sdhci_writew(struct sdhci_host *host, u16 val, int reg)
 
 static inline void sdhci_writeb(struct sdhci_host *host, u8 val, int reg)
 {
-	if (unlikely(host->ops->write_b))
+	if (host->ops->write_b)
 		host->ops->write_b(host, val, reg);
 	else
 		writeb(val, host->ioaddr + reg);
@@ -309,7 +309,7 @@ static inline void sdhci_writeb(struct sdhci_host *host, u8 val, int reg)
 
 static inline u32 sdhci_readl(struct sdhci_host *host, int reg)
 {
-	if (unlikely(host->ops->read_l))
+	if (host->ops->read_l)
 		return host->ops->read_l(host, reg);
 	else
 		return readl(host->ioaddr + reg);
@@ -317,7 +317,7 @@ static inline u32 sdhci_readl(struct sdhci_host *host, int reg)
 
 static inline u16 sdhci_readw(struct sdhci_host *host, int reg)
 {
-	if (unlikely(host->ops->read_w))
+	if (host->ops->read_w)
 		return host->ops->read_w(host, reg);
 	else
 		return readw(host->ioaddr + reg);
@@ -325,7 +325,7 @@ static inline u16 sdhci_readw(struct sdhci_host *host, int reg)
 
 static inline u8 sdhci_readb(struct sdhci_host *host, int reg)
 {
-	if (unlikely(host->ops->read_b))
+	if (host->ops->read_b)
 		return host->ops->read_b(host, reg);
 	else
 		return readb(host->ioaddr + reg);
