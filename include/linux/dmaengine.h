f4c73158b956c6b3b3727a3b682babd5c72d655b
Simon Arlott <sa.me.uk>
2015-10-30 07:54:15 +0000

    dmaengine: bcm63xx-iudma: Add BCM63xx IUDMA Controller
    Signed-off-by: Simon Arlott <simon@fire.lp0.eu>

--- include/linux/dmaengine.h
+++ include/linux/dmaengine.h
@@ -791,6 +791,23 @@ static inline struct dma_async_tx_descriptor *dmaengine_prep_rio_sg(
 }
 #endif
 
+#ifdef CONFIG_BCM63XX_IUDMA
+struct bcm63xx_iudma_context;
+static inline struct dma_async_tx_descriptor *dmaengine_prep_bcm63xx_single(
+	struct dma_chan *chan, dma_addr_t buf, size_t len,
+	enum dma_transfer_direction dir, unsigned long flags,
+	struct bcm63xx_iudma_context *context)
+{
+	struct scatterlist sg;
+	sg_init_table(&sg, 1);
+	sg_dma_address(&sg) = buf;
+	sg_dma_len(&sg) = len;
+
+	return chan->device->device_prep_slave_sg(chan, &sg, 1,
+						  dir, flags, context);
+}
+#endif
+
 static inline struct dma_async_tx_descriptor *dmaengine_prep_dma_cyclic(
 		struct dma_chan *chan, dma_addr_t buf_addr, size_t buf_len,
 		size_t period_len, enum dma_transfer_direction dir,
