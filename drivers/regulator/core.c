6f04f7701b75a8e4b2b5da1c29f6881838c21681
Simon Arlott <sa.me.uk>
2015-11-21 21:05:38 +0000

    -Werror

--- drivers/regulator/core.c
+++ drivers/regulator/core.c
@@ -139,7 +139,7 @@ static bool have_full_constraints(void)
 static void regulator_lock_supply(struct regulator_dev *rdev)
 {
 	struct regulator *supply;
-	int i = 0;
+	int i __attribute__((unused)) = 0;
 
 	while (1) {
 		mutex_lock_nested(&rdev->mutex, i++);
